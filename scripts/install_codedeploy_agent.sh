eea244ad9fb79258ba8933aaa02466e08bd65040
ramagurijala882 <r.gurijala882@mybvc.ca>
2024-11-20 14:00:50 -0700

    Update install_codedeploy_agent.sh

--- scripts/install_codedeploy_agent.sh
+++ scripts/install_codedeploy_agent.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-
-# Install the CodeDeploy agent
-sudo yum update -y
-sudo yum install -y ruby wget
-
-# Download the latest version of the CodeDeploy agent
-cd /tmp
+# Install CodeDeploy agent
+yum install -y ruby
+yum install -y wget
 wget https://github.com/aws/aws-codedeploy-agent/releases/download/latest/codedeploy-agent-1.7.1-1.x86_64.rpm
-
-# Install the CodeDeploy agent
-sudo rpm -i codedeploy-agent-1.7.1-1.x86_64.rpm
-
-# Start the CodeDeploy agent
-sudo service codedeploy-agent start
-
-# Enable the agent to start on boot
-sudo systemctl enable codedeploy-agent
-
-# Check the status of the agent
-sudo service codedeploy-agent status
+rpm -i codedeploy-agent-1.7.1-1.x86_64.rpm
+systemctl enable codedeploy-agent
+systemctl start codedeploy-agent
